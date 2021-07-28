0476b5cdb958b621a2948c27acd374b64b1f903b
Xenon <gonda.armand@gmail.com>
2021-07-28 18:56:14 +0200

    fix: correctly parse gear icons

--- src/components/GearList.jsx
+++ src/components/GearList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Avatar from '@material-ui/core/Avatar';
+// import ImageIcon from '@material-ui/icons/Image';
+// import WorkIcon from '@material-ui/icons/Work';
+// import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
@@ -33,7 +33,7 @@ export default function GearList(props) {
                     return(
                         <ListItem key={`gearPiece_${i}`}>
                             <ListItemAvatar>
-                              <img src={gearPiece.Item.Icon} alt="Gear Piece" />
+                              <img src={`https://xivapi.com${gearPiece.Item.Icon}`} alt="Gear Piece" />
                                 
                                {console.log(gearPiece.Item.Icon)}
                             </ListItemAvatar>
@@ -55,33 +55,33 @@ export default function GearList(props) {
     )
 }
 
-  return (
-    <List className={classes.root}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
-    </List>
-  );
+  // return (
+  //   <List className={classes.root}>
+  //     <ListItem>
+  //       <ListItemAvatar>
+  //         <Avatar>
+  //           <ImageIcon />
+  //         </Avatar>
+  //       </ListItemAvatar>
+  //       <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+  //     </ListItem>
+  //     <ListItem>
+  //       <ListItemAvatar>
+  //         <Avatar>
+  //           <WorkIcon />
+  //         </Avatar>
+  //       </ListItemAvatar>
+  //       <ListItemText primary="Work" secondary="Jan 7, 2014" />
+  //     </ListItem>
+  //     <ListItem>
+  //       <ListItemAvatar>
+  //         <Avatar>
+  //           <BeachAccessIcon />
+  //         </Avatar>
+  //       </ListItemAvatar>
+  //       <ListItemText primary="Vacation" secondary="July 20, 2014" />
+  //     </ListItem>
+  //   </List>
+  // );
 }
 
