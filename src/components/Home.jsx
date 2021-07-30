2c4f2e7cfca6119d66f66b79a3237396cc8deb5a
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-07-30 07:46:22 -0600

    Added Attributes bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,71 +1,70 @@
+import { Paper } from "@material-ui/core";
+import React, { useState } from "react";
 
-import { Paper } from '@material-ui/core';
-import React, { useState } from 'react';
-
-import CharacterModel from './CharacterModel';
-import GearList from './GearList';
-import PersonData from './PersonData';
-import PersonMetaInformation from './PersonMetaInformation';
-import SideBar from './SideBar';
-import Stats from './Stats';
+import CharacterModel from "./CharacterModel";
+import GearList from "./GearList";
+import PersonData from "./PersonData";
+import PersonMetaInformation from "./PersonMetaInformation";
+import SideBar from "./SideBar";
+import Stats from "./Stats";
 
 export default function Home() {
-	const [selectedIndex, setSelectedIndex] = useState(0);
-	const [FFXIVData, setFFXIVData] = useState(undefined);
-	const raidGroupIdentities = {
-		rikkasId: 30071260,
-		lirasId: 24633600,
-		kyarysId: 23829654,
-		mitsusId: 26393450,
-		ezmisId: 13343103,
-		braxtonsId: 27563488,
-		uhrasId: 31478998,
-		yassinsId: 26668179,
-	};
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [FFXIVData, setFFXIVData] = useState(undefined);
+  const raidGroupIdentities = {
+    rikkasId: 30071260,
+    lirasId: 24633600,
+    kyarysId: 23829654,
+    mitsusId: 26393450,
+    ezmisId: 13343103,
+    braxtonsId: 27563488,
+    uhrasId: 31478998,
+    yassinsId: 26668179,
+  };
 
-	return (
-		<>
-			{/* sidebar */}
+  return (
+    <>
+      {/* sidebar */}
 
-			<Paper className="home-container" square>
-				<div className="sidebar-container">
-					<SideBar
-						FFXIVData={FFXIVData}
-						setFFXIVData={setFFXIVData}
-						selectedIndex={selectedIndex}
-						setSelectedIndex={setSelectedIndex}
-						raidGroupIdentities={raidGroupIdentities}
-					/>
-				</div>
-				{/* main content */}
+      <Paper className="home-container" square>
+        <div className="sidebar-container">
+          <SideBar
+            FFXIVData={FFXIVData}
+            setFFXIVData={setFFXIVData}
+            selectedIndex={selectedIndex}
+            setSelectedIndex={setSelectedIndex}
+            raidGroupIdentities={raidGroupIdentities}
+          />
+        </div>
+        {/* main content */}
 
-				<div className="home-content-container">
-					<div className="home-content">
-						{/* left side of main content */}
-						<div className="home-left-side">
-							<div className="home-left-side-content">
-								<div className="person-meta-information-container">
-									<PersonMetaInformation FFXIVData={FFXIVData} />
-								</div>
-								<div className="person-data-container">
-									<PersonData FFXIVData={FFXIVData} />
-								</div>
-							</div>
-						</div>
+        <div className="home-content-container">
+          <div className="home-content">
+            {/* left side of main content */}
+            <div className="home-left-side">
+              <div className="home-left-side-content">
+                <div className="person-meta-information-container">
+                  <PersonMetaInformation FFXIVData={FFXIVData} />
+                </div>
+                <div className="person-data-container">
+                  <PersonData FFXIVData={FFXIVData} />
+                </div>
+              </div>
+            </div>
 
-						{/* right side of main content */}
-						<div className="home-right-side">
-							<div className="char-model-and-gear-container">
-								<CharacterModel FFXIVData={FFXIVData} />
-								<GearList FFXIVData={FFXIVData} />
-							</div>
-							<div className="char-stats-container">
-								<Stats FFXIVData={FFXIVData} />
-							</div>
-						</div>
-					</div>
-				</div>
-			</Paper>
-		</>
-	);
+            {/* right side of main content */}
+            <div className="home-right-side">
+              <div className="char-model-and-gear-container">
+                <CharacterModel FFXIVData={FFXIVData} />
+                <GearList FFXIVData={FFXIVData} />
+              </div>
+              <div className="char-stats-container">
+                <Stats FFXIVData={FFXIVData} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Paper>
+    </>
+  );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,57 +1,57 @@
-import {
-	Typography,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Paper,
-	makeStyles,
-} from '@material-ui/core';
-import React from 'react';
-
-const useStyles = makeStyles({
-	table: {
-		minWidth: 450,
-	},
-});
+import { Typography, Paper } from "@material-ui/core";
+import React from "react";
 
 export default function Stats(props) {
-	const classes = useStyles();
-
-	if (props.FFXIVData) {
-		return (
-			<div className="char-stats-content">
-				<TableContainer component={Paper}>
-					<Table className={classes.table} aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell align="center">Attributes</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{Object.values(props.FFXIVData.Character.GearSet.Attributes).map(attributeInfo => {
-								return Object.values(attributeInfo).map(key => {
-									return (
-										<TableRow key={key.Attribute.Name}>
-											<TableCell component="th" scope="row">
-												{key.Value}
-											</TableCell>
-										</TableRow>
-									);
-								});
-							})}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</div>
-		);
-	}
-
-	return (
-		<div className="char-stats-content">
-			<Typography variant="h3">No stats to show yet..</Typography>
-		</div>
-	);
+  const allowedAttributes = [
+    "Vitality",
+    "Strength",
+    "Dexterity",
+    "Intelligence",
+    "Critical Hit",
+    "Determination",
+    "Direct Hit Rate",
+    "Spell Speed",
+    "Mind",
+    "Piety",
+    "Tenacity",
+  ];
+  if (props.FFXIVData) {
+    //
+    return (
+      <Paper className="char-stats-content">
+        <Typography className="char-stats-attributes">Attributes</Typography>
+        <div className="char-stats-name-and-value-container">
+          {Object.values(props.FFXIVData.Character.GearSet.Attributes).map(
+            (attributeInfo, i) => {
+              return (
+                <div className="char-stats-name-and-value">
+                  {allowedAttributes.forEach((allowedAttribute) => {
+                    Object.values(attributeInfo).map((key, i) => {
+                      if (key.Name == allowedAttribute) {
+                        console.log(key.Name);
+                        return (
+                          <Typography className="char-stats-name">
+                            {key.Name}
+                          </Typography>
+                        );
+                      }
+                      <Typography className="char-stats-value">
+                        {attributeInfo.Value}
+                      </Typography>;
+                    });
+                  })}
+                </div>
+              );
+            }
+          )}
+        </div>
+      </Paper>
+    );
+  } else {
+    return (
+      <div className="char-stats-content">
+        <Typography variant="h3">No stats to show yet..</Typography>
+      </div>
+    );
+  }
 }
