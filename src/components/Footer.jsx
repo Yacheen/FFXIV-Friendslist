056351b663c344c57ee68824a007cee9e8d1462f
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-08-10 00:22:01 -0600

    added footer social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import { Typography, Paper, useMediaQuery } from "@material-ui/core";
 import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import EmailIcon from "@material-ui/icons/Email";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import DiscordLogo from "../Images/discordLogo.png";
 
 export default function Footer() {
     const footerTextPhoneView = useMediaQuery("(max-width: 740px)");
@@ -20,9 +25,21 @@ export default function Footer() {
                         )}
                     </div>
                     <div className="footer-social-links">
-                        <div className="twitter-link" />
-                        <div className="github-link" />
-                        <div className="email-link" />
+                        <div className="discord-link">
+                            <DiscordLogo />
+                        </div>
+                        <div className="linkedin-link">
+                            <LinkedInIcon />
+                        </div>
+                        <div className="twitter-link">
+                            <TwitterIcon />
+                        </div>
+                        <div className="github-link">
+                            <GitHubIcon />
+                        </div>
+                        <div className="email-link">
+                            <EmailIcon />
+                        </div>
                     </div>
                 </div>
             </Paper>
