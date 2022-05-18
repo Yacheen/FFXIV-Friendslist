347a093d8ac1ecba90300ff6dab7c71f2752882f
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-05-18 10:46:31 -0600

    Fixed error handling when user isn't in a guild

--- src/components/GuildData.jsx
+++ src/components/GuildData.jsx
@@ -1,207 +1,240 @@
 import React from "react";
 import {
-    Card,
-    CardContent,
-    Typography,
-    makeStyles,
-    CardActions,
-    Button,
-    Paper,
-    Grid,
-    Tooltip,
-    Hidden,
+	Card,
+	CardContent,
+	Typography,
+	makeStyles,
+	CardActions,
+	Button,
+	Paper,
+	Grid,
+	Tooltip,
+	Hidden,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: 450,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        maxHeight: "55vh",
-        overflow: "auto",
-    },
-    guildInfo: {
-        display: "flex",
-        justifyContent: "space-between",
-        marginBottom: "4rem",
-    },
-    guildMetaInformation: {
-        display: "flex",
-        flexDirection: "column",
-    },
-    buttonStyling: {
-        backgroundColor: theme.palette.primary.light,
-        fontSize: 14,
-    },
-    quoteStyling: {
-        color: theme.palette.secondary.light,
-    },
-    focusStyling: {
-        marginBottom: "1rem",
-    },
-    focusItemStyling: {
-        opacity: 0.2,
-    },
-    tooltipStyling: {
-        color: "primary",
-        fontSize: 20,
-    },
-    focusContainerStyling: {
-        padding: "2rem",
-    },
-    [theme.breakpoints.down(theme.breakpoints.values.laptop)]: {
-        root: {
-            width: 350,
-            height: 417,
-        },
-    },
-    [theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
-        root: {
-            height: 375,
-            overflow: "hidden",
-        },
-    },
-    [theme.breakpoints.down(theme.breakpoints.values.phone)]: {
-        root: {
-            width: 300,
-            minHeight: 575,
-        },
-    },
+	root: {
+		maxWidth: 450,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-between",
+		maxHeight: "55vh",
+		overflow: "auto",
+	},
+	guildInfo: {
+		display: "flex",
+		justifyContent: "space-between",
+		marginBottom: "4rem",
+	},
+	guildMetaInformation: {
+		display: "flex",
+		flexDirection: "column",
+	},
+	buttonStyling: {
+		backgroundColor: theme.palette.primary.light,
+		fontSize: 14,
+	},
+	quoteStyling: {
+		color: theme.palette.secondary.light,
+	},
+	focusStyling: {
+		marginBottom: "1rem",
+	},
+	focusItemStyling: {
+		opacity: 0.2,
+	},
+	tooltipStyling: {
+		color: "primary",
+		fontSize: 20,
+	},
+	focusContainerStyling: {
+		padding: "2rem",
+	},
+	[theme.breakpoints.down(theme.breakpoints.values.laptop)]: {
+		root: {
+			width: 350,
+			height: 417,
+		},
+	},
+	[theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
+		root: {
+			height: 375,
+			overflow: "hidden",
+		},
+	},
+	[theme.breakpoints.down(theme.breakpoints.values.phone)]: {
+		root: {
+			width: 300,
+			minHeight: 575,
+		},
+	},
 }));
 
 export default function GuildData({
-    setSelectedMetaInformation,
-    selectedMetaInformation,
-    FFXIVData,
-    metaInfoLoading,
-    setMetaInfoLoading,
-    setButtonPressed,
+	setSelectedMetaInformation,
+	selectedMetaInformation,
+	FFXIVData,
+	metaInfoLoading,
+	setMetaInfoLoading,
+	setButtonPressed,
 }) {
-    const getGuildMembers = async (buttonClicked, characterId) => {
-        setMetaInfoLoading(true);
-        const response = await fetch(
-            `https://xivapi.com/character/${characterId}?data=${buttonClicked}`
-        );
-        const data = await response.json();
-        setSelectedMetaInformation(data);
-        setButtonPressed(buttonClicked);
-        setMetaInfoLoading(false);
-    };
-    const classes = useStyles();
-    return (
-        <Card className={`${classes.root}`}>
-            <Typography variant="h3" color="primary" align="center">
-                {selectedMetaInformation.FreeCompany.Name}
-            </Typography>
-            <Typography
-                variant="h4"
-                align="center"
-                color="primary"
-                classes={{
-                    colorPrimary: classes.quoteStyling,
-                }}
-            >
-                " {selectedMetaInformation.FreeCompany.Slogan}"
-            </Typography>
-            <CardContent>
-                <div className={classes.guildInfo}>
-                    <div className={classes.guildMetaInformation}>
-                        <Typography variant="h4">
-                            Members:{" "}
-                            {
-                                selectedMetaInformation.FreeCompany
-                                    .ActiveMemberCount
-                            }
-                        </Typography>
-                        <Typography variant="h4">
-                            Server: {selectedMetaInformation.FreeCompany.Server}
-                        </Typography>
-                        <Typography variant="h4">
-                            Rank: {selectedMetaInformation.FreeCompany.Rank}
-                        </Typography>
-                    </div>
-                    <div className={classes.guildButton}>
-                        <Button
-                            size="medium"
-                            variant="contained"
-                            classes={{
-                                contained: classes.buttonStyling,
-                            }}
-                            disabled={metaInfoLoading}
-                            onClick={() =>
-                                getGuildMembers("FCM", FFXIVData.Character.ID)
-                            }
-                        >
-                            View members
-                        </Button>
-                    </div>
-                </div>
+	const getGuildMembers = async (buttonClicked, characterId) => {
+		setMetaInfoLoading(true);
+		const response = await fetch(
+			`https://xivapi.com/character/${characterId}?data=${buttonClicked}`
+		);
+		const data = await response.json();
+		setSelectedMetaInformation(data);
+		setButtonPressed(buttonClicked);
+		setMetaInfoLoading(false);
+	};
+	const classes = useStyles();
+	return (
+		<Card className={`${classes.root}`}>
+			{selectedMetaInformation.FreeCompany ? (
+				<>
+					<Typography variant="h3" color="primary" align="center">
+						{selectedMetaInformation.FreeCompany.Name}
+					</Typography>
+					<Typography
+						variant="h4"
+						align="center"
+						color="primary"
+						classes={{
+							colorPrimary: classes.quoteStyling,
+						}}
+					>
+						" {selectedMetaInformation.FreeCompany.Slogan}"
+					</Typography>
+					<CardContent>
+						<div className={classes.guildInfo}>
+							<div className={classes.guildMetaInformation}>
+								<Typography variant="h4">
+									Members:{" "}
+									{
+										selectedMetaInformation.FreeCompany
+											.ActiveMemberCount
+									}
+								</Typography>
+								<Typography variant="h4">
+									Server:{" "}
+									{selectedMetaInformation.FreeCompany.Server}
+								</Typography>
+								<Typography variant="h4">
+									Rank:{" "}
+									{selectedMetaInformation.FreeCompany.Rank}
+								</Typography>
+							</div>
+							<div className={classes.guildButton}>
+								<Button
+									size="medium"
+									variant="contained"
+									classes={{
+										contained: classes.buttonStyling,
+									}}
+									disabled={metaInfoLoading}
+									onClick={() =>
+										getGuildMembers(
+											"FCM",
+											FFXIVData.Character.ID
+										)
+									}
+								>
+									View members
+								</Button>
+							</div>
+						</div>
 
-                <Typography variant="h4" className={classes.focusStyling}>
-                    Focus
-                </Typography>
-                <Paper
-                    variant="outlined"
-                    square
-                    className={classes.focusContainerStyling}
-                >
-                    <Grid container spacing={3}>
-                        {selectedMetaInformation.FreeCompany.Focus.map(
-                            (focusedItem) => {
-                                //for each focused item, make a grid item
-                                if (focusedItem.Status === true) {
-                                    return (
-                                        <Grid item xs>
-                                            <Tooltip
-                                                classes={{
-                                                    tooltip:
-                                                        classes.tooltipStyling,
-                                                }}
-                                                arrow={true}
-                                                title={`${focusedItem.Name}`}
-                                                placement="top"
-                                            >
-                                                <img
-                                                    src={focusedItem.Icon}
-                                                    alt={focusedItem.Name}
-                                                />
-                                            </Tooltip>
-                                        </Grid>
-                                    );
-                                } else {
-                                    //return a low opacity focus
-                                    return (
-                                        <Grid
-                                            item
-                                            xs
-                                            classes={{
-                                                item: classes.focusItemStyling,
-                                            }}
-                                        >
-                                            <Tooltip
-                                                classes={{
-                                                    tooltip:
-                                                        classes.tooltipStyling,
-                                                }}
-                                                arrow={true}
-                                                title={`${focusedItem.Name}`}
-                                                placement="top"
-                                            >
-                                                <img
-                                                    src={focusedItem.Icon}
-                                                    alt={focusedItem.Name}
-                                                />
-                                            </Tooltip>
-                                        </Grid>
-                                    );
-                                }
-                            }
-                        )}
-                    </Grid>
-                </Paper>
-            </CardContent>
-            <CardActions></CardActions>
-        </Card>
-    );
+						<Typography
+							variant="h4"
+							className={classes.focusStyling}
+						>
+							Focus
+						</Typography>
+						<Paper
+							variant="outlined"
+							square
+							className={classes.focusContainerStyling}
+						>
+							<Grid container spacing={3}>
+								{selectedMetaInformation.FreeCompany.Focus.map(
+									(focusedItem) => {
+										//for each focused item, make a grid item
+										if (focusedItem.Status === true) {
+											return (
+												<Grid item xs>
+													<Tooltip
+														classes={{
+															tooltip:
+																classes.tooltipStyling,
+														}}
+														arrow={true}
+														title={`${focusedItem.Name}`}
+														placement="top"
+													>
+														<img
+															src={
+																focusedItem.Icon
+															}
+															alt={
+																focusedItem.Name
+															}
+														/>
+													</Tooltip>
+												</Grid>
+											);
+										} else {
+											//return a low opacity focus
+											return (
+												<Grid
+													item
+													xs
+													classes={{
+														item: classes.focusItemStyling,
+													}}
+												>
+													<Tooltip
+														classes={{
+															tooltip:
+																classes.tooltipStyling,
+														}}
+														arrow={true}
+														title={`${focusedItem.Name}`}
+														placement="top"
+													>
+														<img
+															src={
+																focusedItem.Icon
+															}
+															alt={
+																focusedItem.Name
+															}
+														/>
+													</Tooltip>
+												</Grid>
+											);
+										}
+									}
+								)}
+							</Grid>
+						</Paper>
+					</CardContent>
+					<CardActions></CardActions>
+				</>
+			) : (
+				<Card className={`${classes.root}`}>
+					<CardContent>
+						<Typography
+							variant="h4"
+							className="person-data-header"
+							align="center"
+						>
+							{selectedMetaInformation.Character.Name} Appears to
+							not be in a guild...
+						</Typography>
+					</CardContent>
+				</Card>
+			)}
+		</Card>
+	);
 }
